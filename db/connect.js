1367acc1a3cce23256c6880b8b5db6a83b29213f
mukthar <mukthar2004@gmail.com>
2024-06-09 00:32:04 +0530

    Initial commit

--- db/connect.js
+++ db/connect.js
@@ -24,7 +24,7 @@ module.exports.get = function() {
   ->This function is assigned to module.exports so that it can be accessed in other files
   ->This function connects to a MongoDB database and takes a callback function (done) as a parameter to indicate when the connection process is finished.  
   ->connected the mongodb using mongodb url 
-  ->
+  ->*/
 
 
 
@@ -40,18 +40,23 @@ const state={
     db:null 
 }
 module.exports.connect=function(done){         
-   const url="mongodb://localhost:27017/";      
+   const url="mongodb://localhost:27017/movie";      
    const dbName="movie";
 
-MongoClient.connect(url,(err,data)=>{
-    if(err) return done(err);                      
-        state.db=data.db(dbName);
+MongoClient.connect(url)
+    .then(() => {
+        console.log("Connected to database");
         done();
-});
-   
+    })
+    .catch((err) => {
+        console.log("Failed to connect to database: " + err);
+        done(err);
+    });
 }
+
 module.exports.get=function(){
-    return state.db}*/
+    return state.db
+}*/
 
 
 
